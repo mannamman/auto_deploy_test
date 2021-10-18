5631f2e14da25f06b0d1a12d3622c1064145d927
namth <wase894@gmail.com>
2021-10-18 10:01:33 +0900

    test

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    env1 = os.environ["env1"]
-    env2 = os.environ["env2"]
+    try:
+        env1 = os.environ["env1"]
+        env2 = os.environ["env2"]
+    except KeyError:
+        return(str(os.environ), 200) 
     env1_type = type(env1)
     env2_type = type(env2)
     return(f"{env1} {env1_type}, {env2} {env2_type}", 200) 
