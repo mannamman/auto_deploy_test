9037f36894155dcf63df44313f30b8c9b4c4b839
namth <wase894@gmail.com>
2021-10-18 09:58:09 +0900

    test

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
-
+import  os
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
 def index():
-    return("Hello world!", 200) 
+    env1 = os.environ["env1"]
+    env2 = os.environ["env2"]
+    env1_type = type(env1)
+    env2_type = type(env2)
+    return(f"{env1} {env1_type}, {env2} {env2_type}", 200) 
 
 
 if(__name__=='__main__'):
